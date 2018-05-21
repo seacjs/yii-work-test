dc8aca12334034b3be784f8fdde05a757a299ee7
seacjs <sea-cjs@yandex.ru>
2018-05-21 14:33:30 +0300

    midified config/db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=192.168.83.137;dbname=yii2basic',
     'username' => 'root',
-    'password' => '1234',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
